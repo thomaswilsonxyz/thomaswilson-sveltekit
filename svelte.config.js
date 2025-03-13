7111a3594596f74aff5cce5dc8a2a84e2905e624
Thomas <thomas@thomaswilson.xyz>
2025-03-13 18:07:12 +0000

    chore: remove mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,12 @@
 import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
-import { mdsvex } from 'mdsvex'
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), 
-		mdsvex({
-		extensions: ['.md'],
-	})],
+	preprocess: [preprocess()],
 
 	kit: {
 		adapter: adapter({ split: false }),
